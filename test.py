96f155df28d373054e3bb1105cf71491d2dee0a4
actions-user <actions@github.com>
2024-01-01 13:03:08 +0000

    action test commit

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-text = "0"
-
-try:
-    with open('test.txt','r') as f:
-        text = f.read()
-    n = int(text[text.rfind('\n')+1:])+1
-    text += '\n'
-    text += str(n)
-except:
-    pass
-
-with open('test.txt','w') as f:
-    f.write(text)
 
 import sys
-print('input test ↓')
-print(sys.argv[1])
+with open(sys.argv[1]+'.txt','w') as f:
+    f.write("hi")
